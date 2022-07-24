adba3ae1a0abba8ce0c1252a7c84248051efeb1a
victorleon31 <victorleon658@gmail.com>
2022-07-24 17:35:34 -0500

    risponse 7 - 8

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,8 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return str
+
   
 }
 
@@ -35,7 +37,8 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  var suma = x + y 
+  return suma  
 }
 
 function resta(x, y) {
